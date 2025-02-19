4c41c1a85f8f51d6f68e26165dec701347395595
VisionOra <sohaibanwaar36@gmail.com>
2025-02-19 19:41:42 +0500

    fix

--- ui/src/components/ui/logging/hooks/index.ts
+++ ui/src/components/ui/logging/hooks/index.ts
@@ -3,11 +3,6 @@ import { useState } from "react";
 export const use = () => {
     const API_BASE_URL =  `${process.env.UI_SERVER_URL || "http://localhost:3000"}/traces`;
 
-
-
-
-;
-
     const [uniqueAgents, setUniqueAgents] = useState<string[]>([]);
     const [uniqueRooms, setUniqueRooms] = useState<string[]>([]);
     const [traceData, setTraceData] = useState<any[]>([]);
